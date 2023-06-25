cb41b1b40651acc0e97688fc3659a276977b8801
VitaliiKonta <120408874+VitaliiKonta@users.noreply.github.com>
2023-06-25 21:48:46 +0300

    Task 2 is ready

--- src/components/Statistic.jsx
+++ src/components/Statistic.jsx
@@ -1,33 +1,43 @@
-// import PropTypes from 'prop-types';
-import { StatSection } from './Statistic.styled';
+import PropTypes from 'prop-types';
 
-function Statistics({ id, lable, percentage }) {
+import {
+  StatSection,
+  StatTitle,
+  StatList,
+  StatItem,
+  StatName,
+  StatPercent,
+} from './Statistic.styled';
+
+function Statistics({ stats, title }) {
   return (
     <StatSection>
-      <h2 class="title">Upload stats</h2>
+      {title && <StatTitle>{title}</StatTitle>}
 
-      <ul class="stat-list">
-        <li class="item">
-          <span class="label">.docx</span>
-          <span class="percentage">4%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp3</span>
-          <span class="percentage">14%</span>
-        </li>
-        <li class="item">
-          <span class="label">.pdf</span>
-          <span class="percentage">41%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp4</span>
-          <span class="percentage">12%</span>
-        </li>
-      </ul>
+      <StatList>
+        {stats.map(statsElem => (
+          <StatItem
+            key={statsElem.id}
+            style={{
+              backgroundColor: 'blue',
+            }}
+          >
+            <StatName>{statsElem.label}</StatName>
+            <StatPercent>{statsElem.percentage}%</StatPercent>
+          </StatItem>
+        ))}
+      </StatList>
     </StatSection>
   );
 }
-
-// Statistics.PropTypes = {};
-
+Statistics.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+  title: PropTypes.string,
+};
 export default Statistics;
--- src/components/Statistic.styled.jsx
+++ src/components/Statistic.styled.jsx
@@ -13,5 +13,40 @@ export const StatSection = styled.section`
   margin-bottom: 100px;
   border-radius: 2px;
   background-color: #fff;
-  color: #fff;
+  color: #d80808;
+`;
+
+export const StatTitle = styled.h2`
+  padding: 16px;
+  color: grey;
+  font-size: 32px;
+`;
+
+export const StatList = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 10px;
+  /* flex-direction: row; */
+  justify-content: space-between;
+  text-align: center;
+  list-style: none;
+  margin: 0;
+  padding-bottom: 16px;
+`;
+
+export const StatItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  width: calc(100% / 5);
+  padding: 15px;
+  border-radius: 1px;
+`;
+export const StatName = styled.span`
+  color: white;
+  font-size: 13px;
+`;
+export const StatPercent = styled.span`
+  color: white;
+  font-size: 16px;
 `;
